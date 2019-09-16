46214cf2efd67c1c3a8f6628f147e567265d8eca
Sujaul Miah <sujaul.m@gmail.com>
2019-09-16 22:00:13 +0100

    Refactor Part B

--- part_b.rb
+++ part_b.rb
@@ -1,29 +1,11 @@
 class SportsTeam
 
+  attr_accessor(:team_name, :players, :coach_name)
+
   def initialize(input_team_name, input_players, input_coach_name)
     @team_name = input_team_name
     @players = input_players
     @coach_name = input_coach_name
   end
 
-  def get_team_name()
-    return @team_name
-  end
-
-  def get_players()
-    return @players
-  end
-
-  def get_coach_name()
-    return @coach_name
-  end
-
-  def set_team_name(new_name)
-    @team_name = new_name
-  end
-
-  def set_coach_name(new_coach)
-    @coach_name = new_coach
-  end
-  
 end
--- specs/part_b_specs.rb
+++ specs/part_b_specs.rb
@@ -7,29 +7,29 @@ class TestSportsTeam< MiniTest::Test
   def test_get_team_name
     team = SportsTeam.new("Tigers", ["Steve", "Tony", "Bruce"], "Sujaul")
     p team
-    assert_equal("Tigers", team.get_team_name())
+    assert_equal("Tigers", team.team_name())
   end
 
   def test_get_players
     team = SportsTeam.new("Tigers", ["Steve", "Tony", "Bruce"], "Sujaul")
-    assert_equal(["Steve", "Tony", "Bruce"], team.get_players())
+    assert_equal(["Steve", "Tony", "Bruce"], team.players())
   end
 
   def test_get_coach_name
     team = SportsTeam.new("Tigers", ["Steve", "Tony", "Bruce"], "Sujaul")
-    assert_equal("Sujaul", team.get_coach_name())
+    assert_equal("Sujaul", team.coach_name())
   end
 
   def test_set_team_name
     team = SportsTeam.new("Tigers", ["Steve", "Tony", "Bruce"], "Sujaul")
-    result = team.set_team_name("Panda")
-    assert_equal("Panda", result)
+    team.team_name = "Panda"
+    assert_equal("Panda", team.team_name())
   end
 
   def test_set_coach_name
     team = SportsTeam.new("Tigers", ["Steve", "Tony", "Bruce"], "Sujaul")
-    result = team.set_coach_name("Karim")
-    assert_equal("Karim", result)
+    team.coach_name = "Karim"
+    assert_equal("Karim", team.coach_name())
   end
 
 end
